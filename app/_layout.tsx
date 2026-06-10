058db6e6c8eb663870294fa61c94702a621ad66e
Jasper Rexx <rexx@Jaspers-MacBook-Air-2.local>
2026-06-11 00:03:53 +0700

    debug

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,32 +1,11 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack, router } from 'expo-router';
+import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-import { onAuthStateChanged } from 'firebase/auth';
 import { useColorScheme } from '@/hooks/use-color-scheme';
-import { auth } from '../firebaseConfig';
-import { useEffect, useState } from 'react';
-import { ActivityIndicator } from 'react-native';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [checking, setChecking] = useState(true);
-
-  useEffect(() => {
-    const unsub = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        router.replace('/(tabs)/home');
-      } else {
-        router.replace('/');
-      }
-      setChecking(false);
-    }
-    );
-    return unsub;
-  }, []);
-
-  if (checking) return <ActivityIndicator style={{ flex: 1 }} />;
-
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
